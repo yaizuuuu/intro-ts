900081cf7ebe48af0fe5a24731a7fe3f19c2fc28
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-10-15 12:21:51 +0900

    イテレータ: inと

---
+++ handbook/iterators_and_generators.js
@@ -0,0 +1,8 @@
+var someArray = [1, 2, 3];
+for (var someIndex in someArray) {
+    console.log(someIndex);
+}
+for (var _i = 0, someArray_1 = someArray; _i < someArray_1.length; _i++) {
+    var some = someArray_1[_i];
+    console.log(some);
+}
---
+++ handbook/iterators_and_generators.ts
@@ -0,0 +1,9 @@
+let someArray = [1, 2, 3];
+
+for (let someIndex in someArray) {
+    console.log(someIndex);
+}
+
+for (let some of someArray) {
+    console.log(some);
+}
